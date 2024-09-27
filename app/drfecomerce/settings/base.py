6be9535ae40980a4d3fdfe16b2bd32914ff7e153
podestal <luisrodriguezpodesta@gmail.com>
2024-09-27 11:42:55 -0400

    drf setup

--- app/drfecomerce/settings/base.py
+++ app/drfecomerce/settings/base.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External Packages
+    'rest_framework',
+    # Internal Apps
 ]
 
 MIDDLEWARE = [
@@ -97,3 +100,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {}
