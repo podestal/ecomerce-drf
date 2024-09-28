6fb5458dee6dbd45d544d31a74acde4493793a50
podestal <luisrodriguezpodesta@gmail.com>
2024-09-28 11:00:38 -0400

    category basic viewset

--- app/drfecomerce/product/views.py
+++ app/drfecomerce/product/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple viewset for viewing all the categories
+    """
+
+    queryset = models.Category.objects.all()
+
+    def list(self, request):
+        serializer = serializers.CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
