03d823d82f02625099baf828210c00ee8a9f73af
podestal <luisrodriguezpodesta@gmail.com>
2024-09-28 11:45:42 -0400

    schema implemented in category list

--- app/drfecomerce/product/views.py
+++ app/drfecomerce/product/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 from . import models
 from . import serializers
@@ -13,6 +14,7 @@ class CategoryViewSet(viewsets.ViewSet):
 
     queryset = models.Category.objects.all()
 
+    @extend_schema(responses=serializers.CategorySerializer)
     def list(self, request):
         serializer = serializers.CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
