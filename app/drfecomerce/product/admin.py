3217ac06c30564a558b620e57a7cd5f322eac75d
podestal <luisrodriguezpodesta@gmail.com>
2024-09-30 14:37:05 -0400

    product line in product admin

--- app/drfecomerce/product/admin.py
+++ app/drfecomerce/product/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from . import models
 
+
+class ProductLineInLine(admin.TabularInline):
+    model = models.ProductLine
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInLine]
+
+
 admin.site.register(models.Category)
 admin.site.register(models.Brand)
-admin.site.register(models.Product)
+admin.site.register(models.ProductLine)
